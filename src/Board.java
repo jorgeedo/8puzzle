a3b73fd552d44221da11d8a99a986a77fe1cbc73
Jorge Edo <jorge.edo@gmail.com>
2012-09-08 21:07:22 +0200

    Initial

---
+++ src/Board.java
@@ -0,0 +1,49 @@
+
+public class Board {
+	// construct a board from an N-by-N array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+	public Board(int[][] blocks){
+	}
+		
+	// board dimension N
+    public int dimension() {
+    	
+    }
+    
+ // number of blocks out of place
+    public int hamming() {
+    	
+    }
+    
+ // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+    	
+    }
+    
+ // is this board the goal board?
+    public boolean isGoal() {
+    	
+    	
+    }
+    
+ // a board obtained by exchanging two adjacent blocks in the same row
+    public Board twin() {
+    	
+    }
+    
+    // does this board equal y?
+    public boolean equals(Object y) {
+    	
+    }
+    
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+    	
+    	
+    }
+    
+    // string representation of the board (in the output format specified below)
+    public String toString() {
+    	
+    }
+}
---
+++ src/Solver.java
@@ -0,0 +1,27 @@
+
+public class Solver {
+	// find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+    	
+    }
+    
+    // is the initial board solvable?
+    public boolean isSolvable() {
+    	
+    }
+    
+    // min number of moves to solve initial board; -1 if no solution
+    public int moves() {
+    	
+    }
+    
+    // sequence of boards in a shortest solution; null if no solution
+    public Iterable<Board> solution() {
+    	
+    }
+    
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
+    	
+    }
+}
